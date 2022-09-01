bd4807547b9d1eeb1b33f6bbeceb5b8f169ca707
Halef Alves <halef.alves@gmail.com>
2022-08-31 23:08:34 -0300

    Pagina Product Datails

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -3,6 +3,7 @@ import Home from 'pages/Home';
 import Navbar from 'components/Navbar';
 import Catalog from 'pages/Catalog';
 import Admin from 'pages/Admin';
+import ProductDetails from 'pages/ProductDetails';
 
 const Rotas = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const Rotas = () => (
     <Routes>
       <Route  path="/" element={<Home />} />
       <Route path="/products" element={<Catalog />} />
+      <Route path="/products/:productId" element={<ProductDetails />} />
       <Route path="/Admin" element={<Admin/>} />
     </Routes>
   </BrowserRouter>
---
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -0,0 +1,41 @@
+import { ReactComponent as Arrow } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+import './styles.css'
+
+const ProductDetails = () => {
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <div className="goback-container">
+          <Arrow />
+          <h2>Voltar</h2>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="Nome do produto"
+              />
+            </div>
+            <div className="name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={2345.67} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do Produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Commodi, perferendis?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
