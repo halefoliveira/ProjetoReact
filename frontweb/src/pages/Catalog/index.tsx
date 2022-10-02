01c58f51cf8a2673402ee2bbef95575e6ab50edb
Halef Oliveira <halef.alves@gmail.com>
2022-10-02 19:39:44 -0300

    Login request

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -4,9 +4,8 @@ import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { SpringPage } from 'types/vendor/spring';
-import { AxiosParams } from 'types/vendor/axios';
 import { BASE_URL } from 'util/requests';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 import './styles.css';
 import CardLoader from './CardLoader';
@@ -17,9 +16,10 @@ const Catalog = () => {
   
 
   useEffect(() => {
-    const params: AxiosParams = {
+    const params: AxiosRequestConfig = {
       method: 'GET',
-      url: `${BASE_URL}/products`,
+      url: "/products",
+      baseURL: BASE_URL,
       params: {
         page: 0,
         size: 12,
